91f7610af5ace69f156df34ce4e5d45264dfd5dc
KrystalBit <97392601+KrystalBit@users.noreply.github.com>
2020-11-01 17:54:34 +0000

    Carga inicial

--- backend/lib/utils.ts
+++ backend/lib/utils.ts
@@ -14,4 +14,29 @@ export function throttledQueue(
     interval = interval / maxRequestsPerInterval
     maxRequestsPerInterval = 1
   }
-  const queue: Array<() => Promise<void>> =
+  const queue: Array<() => Promise<void>> = []
+  let lastIntervalStart = 0
+  let numRequestsPerInterval = 0
+  let timeout: NodeJS.Timeout | undefined
+  /**
+   * Gets called at a set interval to remove items from the queue.
+   * This is a self-adjusting timer, since the browser's setTimeout is highly inaccurate.
+   */
+  const dequeue = () => {
+    const intervalEnd = lastIntervalStart + interval
+    const now = Date.now()
+    /**
+     * Adjust the timer if it was called too early.
+     */
+    if (now < intervalEnd) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      timeout !== undefined && clearTimeout(timeout)
+      timeout = setTimeout(dequeue, intervalEnd - now)
+      return
+    }
+    lastIntervalStart = now
+    numRequestsPerInterval = 0
+    for (const callback of queue.splice(0, maxRequestsPerInterval)) {
+      numRequestsPerInterval++
+      void callback()
+    }
