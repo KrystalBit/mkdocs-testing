3c3ccad88b0dceb0ba1c27f28b7d8838cdc74292
KrystalBit <97392601+KrystalBit@users.noreply.github.com>
2021-01-29 14:22:03 +0000

    Fixed doxygen warnings

--- frontend/modules/model.ts
+++ frontend/modules/model.ts
@@ -5,4 +5,15 @@ export const install = async () => {
   const command = Command.sidecar('node-bin/backend')
   command.on('close', (data) => {
     console.log(
-      `Node backend finished 
+      `Node backend finished with code ${data.code} and signal ${data.signal}`
+    )
+  })
+  command.on('error', (error) =>
+    console.error(`Node backend error: "${error}"`)
+  )
+  command.stderr.on('data', console.warn)
+
+  const child = await command.spawn()
+
+  window.model = initModel({ command, child })
+}
