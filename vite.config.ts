b1631ddae08a4ff97d06d5dfdbb5293b131c16fb
KrystalBit <97392601+KrystalBit@users.noreply.github.com>
2023-04-03 19:32:27 +0000

    Amend README

--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,40 @@ export default defineConfig({
   resolve: {
     alias: {
       '~/': fileURLToPath(new URL('frontend/', import.meta.url)),
-      '~
+      '~b/': fileURLToPath(new URL('backend/', import.meta.url)),
+      '~s/': fileURLToPath(new URL('scripts/', import.meta.url)),
+    },
+  },
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    strictPort: true,
+  },
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+  // env variables
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        frontend: resolve(__dirname, './index.html'),
+        backend: resolve(__dirname, 'backend/index.ts'),
+      },
+      output: [
+        {
+          dir: 'dist',
+        },
+        {
+          dir: 'dist',
+          entryFileNames: '[name].js',
+          format: 'cjs',
+        },
+      ],
+    },
+  },
+  css: {
+    postcss,
+  },
+})
