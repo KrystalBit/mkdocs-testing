2c891f4731da20c4fcd6fc91389b8451ed578d36
KrystalBit <97392601+KrystalBit@users.noreply.github.com>
2023-04-03 06:34:13 +0000

    Update admin.php

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+
+// Tailwind IntelliSense extension still doesn't support ESM
+// Thus this duplicated file that will hopefully disappear at some point
+module.exports = {
+  content: [
+    './index.html',
+    './frontend/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
